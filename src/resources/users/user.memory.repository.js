015ee66093c417ffd4ae97abfd41962db162837c
Uladzimir Zubovich <Uladzimir_Zubovich@epam.com>
2021-05-13 11:27:09 +0300

    feat: add users routers

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,45 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+const User = require('./user.model');
 
-module.exports = { getAll };
+const allUsers = [new User({name: 'admin', login:'admin', password:'admin'})];
+
+const getAll = async () => 
+   allUsers
+;
+
+const getById = async (id) => allUsers.find(user => user.id === id);
+
+const createUser = async ({name, login, password}) => {
+  const user = new User({name, login, password })
+  allUsers.push(user);
+  return user;
+} 
+
+
+const deleteById = async(id) => {
+  const userPosition = allUsers.findIndex(user => user.id === id);
+
+
+  if(userPosition === -1) {
+    throw new Error('User not found');
+  } else {
+    allUsers.splice(userPosition, 1);
+  }
+}
+
+const updateById = async({id, name, login, password }) => {
+  const userPosition = allUsers.findIndex(user => user.id === id);
+
+  if(userPosition === -1) {
+    throw new Error('User not found');
+  } else {
+    const oldUser = allUsers[userPosition];
+    const newUser = {...oldUser, name, login, password };
+
+    allUsers.splice(userPosition, 1, newUser);
+    return newUser;
+  }
+}
+
+
+
+module.exports = { allUsers, getAll, getById, createUser, deleteById, updateById };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const getById = (id) => usersRepo.getById(id);
+const createUser = ({name, login, password}) => usersRepo.createUser({name, login, password});
+const deleteById = (id) => usersRepo.deleteById(id);
+const updateById = ({id, name, login, password}) => usersRepo.updateById({id, name, login, password});
 
-module.exports = { getAll };
+module.exports = { getAll, getById, createUser, deleteById, updateById };
