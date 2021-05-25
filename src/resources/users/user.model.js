a1a48166ceb32a9cf270f0400be481c1845a35e9
Uladzimir Zubovich <Uladzimir_Zubovich@epam.com>
2021-05-25 11:20:10 +0300

    docs: add users typing

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,18 +1,41 @@
 const {v4: uuid} = require('uuid');
 
+/**
+ * @typedef {Object} UserParams
+ * @property {string} name User name
+ * @property {string} login User login
+ * @property {string} password User password
+ */
+
+/**
+ * @typedef {UserParams & {id: string}} UserModel User model compose properties defined in UserParams and id
+ */
+
+/**
+ * Class representing a user
+ */
 class User {
+    /**
+     * Create a user.
+     * @param {UserParams} newUser parameters for new user
+     * @returns {UserModel} new user
+     */
   constructor({
-    id = uuid(),
     name = 'USER',
     login = 'user',
     password = 'P@55w0rd'
   } = {}) {
-    this.id = id;
+    this.id = uuid();
     this.name = name;
     this.login = login;
     this.password = password;
   }
 
+  /**
+   * Return safe data of user
+   * @param {UserModel} user User model
+   * @returns {{id: string; name: string; login: string}} User parameters from UserModel
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
