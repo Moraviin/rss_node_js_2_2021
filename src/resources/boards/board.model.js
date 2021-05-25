88c672521b7af9ac0bf2a4fdcb5bf23098fe4fa2
Uladzimir Zubovich <Uladzimir_Zubovich@epam.com>
2021-05-25 11:34:01 +0300

    docs: add boards typing

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,16 +1,43 @@
-const {v4: uuid} = require('uuid');
+const { v4: uuid } = require('uuid');
 
+/**
+ * @typedef {Object} ColumnsModel
+ * @property {string} id column id
+ * @property {string} title column title
+ * @property {string} order column order
+ */
+
+
+/**
+ * @typedef {Object} BoardParams
+ * @property {string} title Board title
+ * @property {Array.<ColumnsModel>} columns Board order
+ */
+
+/**
+ * @typedef {BoardParams & {id: string}} BoardModel Board model compose properties defined in BoardParams and id
+ */
+
+/**
+ * Class representing a board
+ */
 class Board {
-  constructor({
-    id = uuid(),
-    title = 'BOARD',
-    columns = []
-  } = {}) {
+  /**
+   * Create a board.
+   * @param {BoardParams} newTask parameters for new board
+   * @returns {BoardModel} new board
+   */
+  constructor({ id = uuid(), title = 'BOARD', columns = [] } = {}) {
     this.id = id;
     this.title = title;
     this.columns = columns;
   }
 
+  /**
+   * Return safe data of board
+   * @param {BoardModel} board board model
+   * @returns {BoardModel} board parameters from UserModel
+   */
   static toResponse(board) {
     const { id, title, columns } = board;
     return { id, title, columns };
